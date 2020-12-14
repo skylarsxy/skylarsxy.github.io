ef542fdf3cd84ae27f9d3aef914bb880929f2d7b
skylarsxy <1155148730@link.cuhk.edu.hk>
2020-12-14 17:51:26 +0800

    assignment

---
+++ assignment05/park.js
@@ -0,0 +1,4 @@
+function table(){
+    var selecter = parent.document.getElementById("selecter");
+                                if (selecter.value=="0"){document.getElementById("iframe").src="https://airtable.com/embed/shrO5umbyT4bjmxV0?backgroundColor=yellow&viewControls=on"};
+                                if (selecter.value=="1"){document.getElementById("iframe").src="https://airtable.com/embed/shrYz28PKrSn0DZoo?backgroundColor=yellow&viewControls=on"};}
